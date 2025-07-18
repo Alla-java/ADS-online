70ac9e15b67e0910ca2d78998dea66e7113c4f27
Alla-java <goneman@yandex.ru>
2025-07-18 18:46:29 +0300

    Добавлены entity класс User, UserMapper и UserRepository

---
+++ src/main/java/ru/skypro/homework/model/User.java
@@ -0,0 +1,57 @@
+package ru.skypro.homework.model;
+
+import liquibase.repackaged.org.apache.commons.lang3.builder.ToStringExclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import ru.skypro.homework.dto.Role;
+import ru.skypro.homework.model.Ad;
+
+import javax.persistence.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToOne
+    @JoinColumn(name = "image_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Image image;
+
+
+    @OneToMany(
+            mappedBy = "author",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    @ToStringExclude
+    private List<Ad> ads;
+
+    public User(String email, String firstName, String lastName, String password, String phone, Role role) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.phone = phone;
+        this.role = role;
+    }
+}
--- src/main/java/ru/skypro/homework/service/UserService.java
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -3,13 +3,13 @@ package ru.skypro.homework.service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPassword;
 import ru.skypro.homework.dto.UpdateUser;
-import ru.skypro.homework.dto.User;
+import ru.skypro.homework.dto.UserDto;
 
 public interface UserService {
 
     void updatePassword(NewPassword passwordDto);
 
-    User getUser(); // новое имя
+    UserDto getUser(); // новое имя
 
     void updateUser(UpdateUser updateDto); // новое имя
 
