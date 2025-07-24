ea0101975767395cca14bd215afbadd60488d4c5
IgorKa45 <igorkanasnik@gmail.com>
2025-07-24 19:38:02 +0400

    Настроил аутентификацию и авторизацию пользователя в БД и доработал класс CommentsServiceImpl

--- src/main/java/ru/skypro/homework/controller/AdController.java
+++ src/main/java/ru/skypro/homework/controller/AdController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.ads.AdDto;
@@ -16,46 +17,51 @@ import ru.skypro.homework.service.AdService;
 @RestController
 @RequestMapping("/ads")
 @RequiredArgsConstructor
-@Tag(name="Объявления")
+@Tag(name = "Объявления")
 
-public class AdController{
+public class AdController {
 
-private final AdService adService;
+    private final AdService adService;
 
-@Operation(summary = "Получение всех объявлений")
-@GetMapping
-public Ads getAllAds(){
-    return adService.getAllAds();
-}
+    @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "Получение всех объявлений")
+    @GetMapping
+    public Ads getAllAds() {
+        return adService.getAllAds();
+    }
 
-@Operation(summary = "Добавление объявления")
-@PostMapping(consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
-public AdDto addAd(@RequestPart("properties") @Valid CreateOrUpdateAd properties,@RequestPart("image") MultipartFile image){
-    return adService.addAd(properties,image);
-}
+    @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "Добавление объявления")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public AdDto addAd(@RequestPart("properties") @Valid CreateOrUpdateAd properties, @RequestPart("image") MultipartFile image) {
+        return adService.addAd(properties, image);
+    }
 
-@Operation(summary = "Получение информации об объявлении")
-@GetMapping("/{id}")
-public ExtendedAdDto getAd(@PathVariable Integer id){
-    return adService.getAd(id);
-}
+    @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "Получение информации об объявлении")
+    @GetMapping("/{id}")
+    public ExtendedAdDto getAd(@PathVariable Integer id) {
+        return adService.getAd(id);
+    }
 
-@Operation(summary = "Удаление объявления")
-@DeleteMapping("/{id}")
-public void deleteAd(@PathVariable Integer id){
-    adService.deleteAd(id);
-}
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isOwnerOfAd(#id)")
+    @Operation(summary = "Удаление объявления")
+    @DeleteMapping("/{id}")
+    public void deleteAd(@PathVariable Integer id) {
+        adService.deleteAd(id);
+    }
 
-@Operation(summary = "Обновление информации об объявлении")
-@PatchMapping("/{id}")
-public AdDto updateAd(@PathVariable Integer id,@RequestBody @Valid CreateOrUpdateAd dto){
-    return adService.updateAd(id,dto);
-}
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isOwnerOfAd(#id)")
+    @Operation(summary = "Обновление информации об объявлении")
+    @PatchMapping("/{id}")
+    public AdDto updateAd(@PathVariable Integer id, @RequestBody @Valid CreateOrUpdateAd dto) {
+        return adService.updateAd(id, dto);
+    }
 
-@Operation(summary = "Получение объявлений авторизованного пользователя")
-@GetMapping("/me")
-public Ads getMyAds(){
-    return adService.getMyAds();
-}
-
+    @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "Получение объявлений авторизованного пользователя")
+    @GetMapping("/me")
+    public Ads getMyAds() {
+        return adService.getMyAds();
+    }
 }
